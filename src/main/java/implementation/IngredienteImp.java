53ff00512fd675b5dbe9037295fa70bdb3ad5e8c
raewashere <55217901+raewashere@users.noreply.github.com>
2024-11-20 00:02:54 -0600

    Add files via upload
    Cambio ruta archivos

---
+++ src/main/java/implementation/IngredienteImp.java
@@ -0,0 +1,12 @@
+package implementation;
+
+import services.IngredienteDAO;
+
+public class IngredienteImp implements IngredienteDAO {
+
+    @Override
+    public void hola() {
+        System.out.println("HOLA");
+    }
+    
+}
